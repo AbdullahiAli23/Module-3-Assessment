e25c5dbbd199257ebd66aab699d770be7de99549
Abdullahi <abdullahali8081@gmail.com>
2023-07-09 22:55:12 -0500

    first commit and finished part 1 and 2

---
+++ about.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	 alert("Form is submitted");
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+const img = document.querySelector("img");
+img.addEventListener("mouseover", function(){
+	alert("Cute cat")
+})
